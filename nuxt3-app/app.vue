4e157815a11b48ea0765cb1af8cd54ed3def6f19
Kazunari-h <kazunario456@gmail.com>
2021-10-22 16:41:39 +0900

    Added Nuxt3 Sample

---
+++ nuxt3-app/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="box">
+    text
+  </div>
+</template>
+
+<style scoped>
+.box {
+  padding: 2rem;
+  margin: 0 auto;
+  background-color: red;
+}
+</style>
+
+<script lang="ts">
+export default {
+  created() {
+    console.log("test");
+    console.log("test");
+    console.log("test");
+    
+  }
+}
+</script>
